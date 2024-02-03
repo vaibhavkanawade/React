908551821ca43596f2a2312e95511cf52c28ca96
vaibhavkanawade <vaibhavkanawade12@gmail.com>
2024-02-03 20:37:31 +0530

    react notes add

---
+++ notes.js
@@ -0,0 +1,77 @@
+
+
+// ➋ JSX
+
+// ➀ What is JSX?
+// ➁ Can a JSX code directly run on a browser?
+// ➂ What do we need to convert JSX code to plain-JS code?
+// ➃ Is "import React from react" necessary? If yes, when?
+// ➄ Write a sample JSX code and convert it to its equivalent JavaScript.
+
+// ➌ Virtual DOM
+
+// ➀ What is a Virtual DOM?
+// ➁ Why does React use a Virtual DOM?
+// ➂ Advantages of Virtual DOM over normal DOM?
+// ➃ Explain how the Virtual DOM works.
+// ➄ When and how are changes propagated to the real DOM?
+
+// ➍ Components
+
+// ➀ What is a component?
+// ➁ How many ways can a component be defined?
+// ➂ How to define a class-based component?
+// ➃ How to define a function-based component?
+// ➄ What are the advantages and disadvantages of defining a component function-based vs. class-based?
+
+// ➅ Explain the life cycle of a component.
+// ➆ What is a life cycle method and what is its purpose?
+// ➇ Give examples of some life cycle methods and their usage.
+
+// ➎ States and Props
+
+// ➀ What is a state?
+// ➁ What is a prop?
+// ➂ Difference between a state and a prop?
+// ➃ What is a stateful component and a stateless component?
+
+// ➄ How states are set in a class-based component?
+// ➅ How states are set in a function-based component?
+// ➆ How to define props in a class-based component?
+// ➇ How to define props in a function-based component?
+// ➈ Explain the behavior when a state's value is changed.
+
+// ➏ Data Binding
+
+// ➀ What is data binding?
+// ➁ What are the different types of data binding?
+// ➂ What kind of data binding does React allow?
+// ➃ Which data is available for binding in React?
+
+// ➐ Component Rendering
+
+// ➀ What do you mean by rendering a component?
+// ➁ Where to define rendering logic in a class-based component v/s function-based component?
+// ➂ What may cause the rendering process slower?
+// ➃ What is rerendering of a component and when does it happen?
+
+// ➑ Introduction to Hooks
+
+// ➀ What is a hook?
+// ➁ Name the hooks which React provides out-of-the-box.
+// ➂ What are the general conventions for naming a hook?
+// ➃ From where can a React hook be called?
+// ➄ Point out a few places where a hook can't be used.
+// ➅ What is useState?
+
+// ➆ Compare useState with the state management in class-based components.
+// ➇ What is useEffect?
+// ➈ Which life cycle methods are internally handled by useEffect?
+// ➉ What is useRef?
+// ➀➀ Give some scenarios where useRef should be used.
+
+// ➀➁ Difference between useState and useRef.
+// ➀➂ What is useContext?
+// ➀➃ Explain how to manage contexts in components and how useContext fits in there.
+// ➀➄ What is memoization and how does React achieve it?
+// ➀➅ Explain the useMemo hook.
