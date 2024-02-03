35b5cf379f16b5e8488c9991303381fb9aff43a9
vaibhavkanawade <vaibhavkanawade12@gmail.com>
2024-02-03 21:17:45 +0530

    notes

--- notes.js
+++ notes.js
@@ -75,3 +75,25 @@
 // ➀➃ Explain how to manage contexts in components and how useContext fits in there.
 // ➀➄ What is memoization and how does React achieve it?
 // ➀➅ Explain the useMemo hook.
+
+
+
+
+
+// Introduction to React
+/*React is an efficient, flexible, and open-source JavaScript  library that allows developers to the creation of simple, fast, and scalable web applications. Jordan Walke, a software engineer who was working for Facebook created React. It was first deployed on the news feed of Facebook in 2011 and on Instagram in 2012. Developers from the Javascript background can easily develop web applications with the help of React.*/
+
+
+
+// 1. What is React?
+// React is a front-end and open-source JavaScript library which is useful in developing user interfaces specifically for applications with a single page. It is helpful in building complex and reusable user interface(UI) components of mobile and web applications as it follows the component-based approach.
+
+// The important features of React are:
+
+// It supports server-side rendering.
+// It will make use of the virtual DOM rather than real DOM (Data Object Model) as RealDOM manipulations are expensive.
+// It follows unidirectional data binding or data flow.
+// It uses reusable or composable UI components for developing the view.
+
+
+
