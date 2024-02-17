970c341ef3083e19627ce9a337375174e2dc6d60
vaibhavkanawade <vaibhavkanawade12@gmail.com>
2024-02-18 00:01:53 +0530

    react basic input add

--- practicesession/src/components/Input.js
+++ practicesession/src/components/Input.js
@@ -2,15 +2,23 @@ import React, { useState } from "react";
 
 function Input(props) {
     function check(e) {
-        props.setInput(e.target.value);
+        console.log(e);
+        if (e.target.id === "firstname") {
+            props.setInput(e.target.value);
+        } else if (e.target.id === "surname") {
+            props.setInput2(e.target.value);
+        } else if (e.target.id === "No") {
+            props.setNum(e.target.value);
+        }
+
     }
 
     return (
         <>
             <div className="form">
-                <input type="text" placeholder="enter name" onChange={check} />
-                <input type="text" placeholder="enter surname" onChange={check} />
-                <input type="number" placeholder="enter number" onChange={check} />
+                <input type="text" id="firstname" placeholder="enter name" onChange={check} />
+                <input type="text" id="surname" placeholder="enter surname" onChange={check} />
+                <input type="number" id="No" placeholder="enter number" onChange={check} />
 
             </div>
 
